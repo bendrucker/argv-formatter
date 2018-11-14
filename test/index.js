935f8a1ce7749ef730b7c533c3f3a470848ef137
Ben Drucker <bvdrucker@gmail.com>
2018-11-13 21:22:55 -0800

    add globals

--- test/index.js
+++ test/index.js
@@ -1,68 +1,68 @@
-'use strict';
+'use strict'
 
-var expect    = require('chai').expect;
-var formatter = require('../');
+var expect = require('chai').expect
+var formatter = require('../')
+
+/* global describe, it */
 
 describe('argv-formatter', function () {
-
   function assert (input, output) {
-    expect(formatter.format(input)).to.deep.equal(output);
+    expect(formatter.format(input)).to.deep.equal(output)
   }
 
   it('can handle single character keys', function () {
-    assert({R: 'spec'}, ['-R', 'spec']);
-  });
+    assert({R: 'spec'}, ['-R', 'spec'])
+  })
 
   it('can handle multi character keys', function () {
-    assert({reporter: 'spec'}, ['--reporter=spec']);
-  });
+    assert({reporter: 'spec'}, ['--reporter=spec'])
+  })
 
   it('removes falsy arguments', function () {
-    assert({reporter: false}, []);
-  });
+    assert({reporter: false}, [])
+  })
 
   it('only includes a flag for true (single)', function () {
-    assert({R: true}, ['-R']);
-  });
+    assert({R: true}, ['-R'])
+  })
 
   it('only includes a flag for true (multi)', function () {
-    assert({reporter: true}, ['--reporter']);
-  });
+    assert({reporter: true}, ['--reporter'])
+  })
 
   it('stringifies object values (single)', function () {
     assert({O: {
       toString: function () {
-        return 'value';
+        return 'value'
       }
-    }}, ['-O', 'value']);
-  });
+    }}, ['-O', 'value'])
+  })
 
   it('stringifies object values (single)', function () {
     assert({O: {
       toString: function () {
-        return 'value';
+        return 'value'
       }
-    }}, ['-O', 'value']);
-  });
+    }}, ['-O', 'value'])
+  })
 
   it('stringifies object values (optionless)', function () {
     assert({_: {
       toString: function () {
-        return 'value';
+        return 'value'
       }
-    }}, ['value']);
-  });
+    }}, ['value'])
+  })
 
   it('can handle one non-option argument', function () {
-    assert({_: 'foo'}, ['foo']);
-  });
+    assert({_: 'foo'}, ['foo'])
+  })
 
   it('can handle multiple non-option arguments', function () {
-    assert({_: ['foo', 'bar']}, ['foo', 'bar']);
-  });
+    assert({_: ['foo', 'bar']}, ['foo', 'bar'])
+  })
 
   it('returns an empty array for an empty object', function () {
-    assert({}, []);
-  });
-
-});
+    assert({}, [])
+  })
+})
